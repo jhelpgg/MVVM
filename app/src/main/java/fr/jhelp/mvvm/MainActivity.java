ca035cb9f17cdc96abe2457fb9ebc1daf082a79b
JHelpGG <jhelpgg@gmail.com>
2022-12-10 14:18:22 +0100

    Calendar UI layout

--- app/src/main/java/fr/jhelp/mvvm/MainActivity.java
+++ app/src/main/java/fr/jhelp/mvvm/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.text.DateFormat;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
